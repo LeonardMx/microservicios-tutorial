1c0f1c9014a01fecece629e093553bda46554c4e
Your Name <you@example.com>
2023-12-20 21:00:22 -0600

    eureka

--- moto-service/src/main/java/com/moto/service/entidades/Moto.java
+++ moto-service/src/main/java/com/moto/service/entidades/Moto.java
@@ -1,9 +1,15 @@
 package com.moto.service.entidades;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+//import jakarta.persistence.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
 
 @Entity
 public class Moto {
