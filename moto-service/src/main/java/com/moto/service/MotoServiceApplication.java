5d70fda5d168fad4aacc4c259ddca55e2694a59d
Your Name <you@example.com>
2023-12-21 20:18:17 -0600

    eureka

--- moto-service/src/main/java/com/moto/service/MotoServiceApplication.java
+++ moto-service/src/main/java/com/moto/service/MotoServiceApplication.java
@@ -3,10 +3,12 @@ package com.moto.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @EnableDiscoveryClient
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class MotoServiceApplication {
 
 	public static void main(String[] args) {
