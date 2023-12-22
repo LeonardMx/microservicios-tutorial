b8e2811362a2544036522512038eda3598939259
Your Name <you@example.com>
2023-12-21 22:46:17 -0600

    eureka

--- eureka-service/src/main/java/com/eureka/service/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/eureka/service/EurekaServiceApplication.java
@@ -1,7 +1,9 @@
 package com.eureka.service;
 
+import com.netflix.discovery.shared.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
